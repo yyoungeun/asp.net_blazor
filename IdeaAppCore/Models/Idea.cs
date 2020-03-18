0003d4fa4ca41855dacb0a9b2f9fe6e7c99cd5b9
yyoungeun <1150amy@naver.com>
2020-03-18 15:36:00 +0900

    blazor commit

---
+++ IdeaAppCore/Models/Idea.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdeaAppCore.Models
+{
+    public class Idea
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string Note { get; set; }
+    }
+}
